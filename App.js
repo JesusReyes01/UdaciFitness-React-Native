8ce061442cfbad28b7f8370dec5851731e13d8ed
Jesus Reyes <jesusareyes01@gmail.com>
2021-03-22 22:35:12 -0500

    test

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
         
         <StatusBar style="auto" />
       </View>
