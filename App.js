6946c4e5aac63e7833b412c4b540240776895cc2
Jesus Reyes <jesusareyes01@gmail.com>
2021-03-23 22:55:55 -0500

    push

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Ionicons name='ios-pizza' color='red' size={100}/>
-        
-        <StatusBar style="auto" />
+        <AddEntry/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
