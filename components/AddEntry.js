e9484dafb15b2ef36a934643541231ac26906b02
Jesus Reyes <jesusareyes01@gmail.com>
2021-03-24 23:35:07 -0500

    added DateHeader component and added DateHeader component to AddEntry component. Also passed in a formatted date prop

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciStepper'
+import DateHeader from './DateHeader'
 
 export default class AddEntry extends Component {
     state = {
@@ -45,6 +46,7 @@ export default class AddEntry extends Component {
 
         return (
             <View>
+                <DateHeader date={(new Date()).toLocaleDateString()}/>
                 {Object.keys(metaInfo).map((key) => {
                     const { getIcon, type, ...rest } = metaInfo[key]
                     const value = this.state[key] 
