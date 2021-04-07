233697ef507f99ad49d094820df5e817a1809c41
Jesus Reyes <jesusareyes01@gmail.com>
2021-04-07 01:20:59 -0500

    had to troubleshoot and remove dependencies to get expo to work as it should

--- App.js
+++ App.js
@@ -6,52 +6,65 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { purple, white } from './utils/colors'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
 
-const Tabs = createBottomTabNavigator({
-  History: {
-    screen: History,
-    navigationOptions: {
-      tabBarLabel: "History",
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    }
-  },
-  AddEntry: {
-    screen: AddEntry,
-    navigationOptions: {
-      tabBarLabel: 'Add Entry',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    }
-  }
-}, {
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? purple : white,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? white : purple,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
+const Tab = 
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator()
 
-export default class App extends React.Component {
+// Redux setup omitted
 
-  render() {
-    return (
-      <Provider store={createStore(reducer)}>  
-        <View style={{flex: 1}}>
-          <View style={{height: 20}} />
-          <Tabs />
-        </View>
-      </Provider>
-    );
-  }
+export default function App() {
+  // Uncomment to reset local data:
+  // AsyncStorage.clear()
+
+  return (
+    <Provider store={createStore(reducer)}>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="AddEntry"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let icon
+              if (route.name === 'Add Entry') {
+                icon = (
+                  <FontAwesome name="plus-square" size={size} color={color} />
+                )
+              } else if (route.name === 'History') {
+                icon = (
+                  <Ionicons name="ios-bookmarks" size={size} color={color} />
+                )
+              }
+              return icon
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: Platform.OS === 'ios' ? purple : white,
+            style: {
+              height: 80,
+              backgroundColor: Platform.OS === "ios" ? white : purple,
+              shadowColor: "rgba(0, 0, 0, 0.24)",
+              shadowOffset: {
+                width: 0,
+                height: 3
+              },
+              shadowRadius: 6,
+              shadowOpacity: 1
+            },
+            indicatorStyle: {
+              // Android tab indicator (line at the bottom of the tab)
+              backgroundColor: 'yellow',
+            },
+          }}
+        >
+          <Tab.Screen name="Add Entry" component={AddEntry} />
+          <Tab.Screen name="History" component={History} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 }
-
